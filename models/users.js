f32a18c1de13ebcb49c256928ab67b0e48e1a495
Executor <just4dotaalex@gmail.com>
2023-03-09 23:47:20 +0200

    feat: add uuid for apiKey

--- models/users.js
+++ models/users.js
@@ -1,9 +1,10 @@
 const { Schema, model } = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  apiKey: { type: String, required: true, unique: true },
+  apiKey: { type: String, required: true, unique: true, default: uuidv4() },
 });
 
 const Users = new model('users', userSchema, 'users');
