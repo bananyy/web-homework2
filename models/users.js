cb72f2788a62f72e1b56b1b6240ee33582f7cd95
Executor <just4dotaalex@gmail.com>
2023-03-10 00:06:11 +0200

    chore: remove uuid

--- models/users.js
+++ models/users.js
@@ -1,10 +1,9 @@
 const { Schema, model } = require('mongoose');
-const { v4: uuidv4 } = require('uuid');
 
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  apiKey: { type: String, required: true, unique: true, default: uuidv4() },
+  apiKey: { type: String, required: true, unique: true },
 });
 
 const Users = new model('users', userSchema, 'users');
