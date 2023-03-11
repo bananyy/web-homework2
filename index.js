42008e8afd84921fd9b761b19fbe7f6bc850da73
Executor <just4dotaalex@gmail.com>
2023-03-11 19:35:44 +0200

    feat: add getUser router

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const setup = require('./DB/mongoose');
 require('dotenv').config();
+const addUserRouter = require('./api/user/addUser');
+const getUserRouter = require('./api/user/getUser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(addUserRouter);
+app.use(getUserRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started ${process.env.PORT}`);
 });
+
+setup();
