019a4114fd0c916c8fb99ab81bcc50db183a0c25
Executor <just4dotaalex@gmail.com>
2023-03-14 22:21:52 +0200

    feat: add addLink router

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const setup = require('./DB/mongoose');
 require('dotenv').config();
 const addUserRouter = require('./api/user/addUser');
 const getUserRouter = require('./api/user/getUser');
+const addLinkRouter = require('./api/links/addLink');
+const getLinkRouter = require('./api/links/getLink');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(addUserRouter);
 app.use(getUserRouter);
+app.use(addLinkRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started ${process.env.PORT}`);
