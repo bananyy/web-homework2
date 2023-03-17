36de1874e7addf4f299265d5e2a5d1b9739f4d04
Executor <just4dotaalex@gmail.com>
2023-03-17 20:25:26 +0200

    fix: fix redirect link

--- api/links/getShortLink.js
+++ api/links/getShortLink.js
@@ -6,20 +6,20 @@ const router = Router();
 router.get('/shortLink/:cut', async (req, res) => {
   const { cut } = req.params;
 
-  const link = await Link.findOne({ 'link.cut': cut })
+  const linkDb = await Link.findOne({ 'link.cut': cut })
     .catch((error) => {
       res.status(400).send(error);
     });
 
-  if (!link) {
+  if (!linkDb) {
     return res.status(400).json({ error: 'Short link was not found' });
   }
 
-  if (new Date() > link.expiredAt) {
+  if (new Date() > linkDb.expiredAt) {
     return res.status(400).json({ error: 'Link was expired' });
   }
 
-  res.status(200).redirect(link.original);
+  res.status(200).redirect(linkDb.link.original);
 });
 
 module.exports = router;
