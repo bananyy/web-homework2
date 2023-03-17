0fbb66d5e7aef51964bfcb8851bb2d5007818f56
Executor <just4dotaalex@gmail.com>
2023-03-17 18:56:35 +0200

    feat: add getShortLink router

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const addUserRouter = require('./api/user/addUser');
 const getUserRouter = require('./api/user/getUser');
 const addLinkRouter = require('./api/links/addLink');
 const getLinkRouter = require('./api/links/getLink');
+const getShortLinkRouter = require('./api/links/getShortLink');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(addUserRouter);
 app.use(getUserRouter);
 app.use(addLinkRouter);
 app.use(getLinkRouter);
+app.use(getShortLinkRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started ${process.env.PORT}`);
